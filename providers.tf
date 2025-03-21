87598574211c354b50a0adc5c3d15c1d26d31a8d
Andi Ashari <a.ashari1302@gmail.com>
2025-03-21 23:22:07 +0700

    refactor: use flexible provider version constraint

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "6.24.0"
+      version = ">= 6.24.0, < 7.0.0"
     }
   }
   required_version = ">= 1.11.1"
